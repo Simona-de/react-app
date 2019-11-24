b28d9ddc46b6b8de8ede33bf39f3c4c51e2fa311
Simona-de <57255935+Simona-de@users.noreply.github.com>
2019-11-24 16:44:43 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 import React from "react";
 import Temperature from "./Temperature";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <body>
-        <div className="Weather-html">
-          <h1 id="city"></h1>
-          <p id="date">Monday 16:00</p>
-          <form id="enter-city">
-            <input
-              type="text"
-              placeholder="enter city"
-              id="city-input"
-              autoComplete="off"
-            />
-            <input type="submit" value="search" />
-          </form>
-          <button id="current-Loc">current location</button>
+      <div className="Weather-html">
+        <h1 id="city"></h1>
+        <p id="date">Monday 16:00</p>
+        <form id="enter-city">
+          <input
+            type="text"
+            placeholder="enter city"
+            id="city-input"
+            autoComplete="off"
+          />
+          <input type="submit" value="search" />
+        </form>
+        <button id="current-Loc">current location</button>
 
-          <Temperature />
+        <Temperature />
 
-          <br />
-          <br />
-          <br />
-          <div className="row weather-forecast" id="forecast"></div>
-          <small>
-            <a href="https://github.com/Simona-de/react-app" target="blank">
-              {" "}
-              Oper source code
-            </a>{" "}
-            by Simona Delogu
-          </small>
-        </div>
-      </body>
+        <br />
+        <br />
+        <br />
+        <div className="row weather-forecast" id="forecast"></div>
+        <small>
+          <a href="https://github.com/Simona-de/react-appnp" target="blank">
+            {" "}
+            Oper source code
+          </a>{" "}
+          by Simona Delogu
+        </small>
+      </div>
     </div>
   );
 }
