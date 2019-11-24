e40ab0853973c9e035cded0ce30cd09981f26494
Simona-de <57255935+Simona-de@users.noreply.github.com>
2019-11-24 21:03:42 +0100

    installed axios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="Weather-html">
-        <h1 id="city"></h1>
+        <h1 id="city">Tokyo</h1>
         <p id="date">Monday 16:00</p>
         <form id="enter-city">
           <input
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,6 +1,9 @@
 import React from "react";
+import App from "./App.css";
+import axios from "axios";
 
 export default function Temperature() {
+  const Apikey = "7012c7fcda50068220d3e6f3e6ec7ff3";
   return (
     <div className="Row">
       <div className="Col-6">
@@ -11,15 +14,15 @@ export default function Temperature() {
               alt=""
               id="icon"
             />
-            <span id="sky"></span>
+            <span id="sky">clear Sky</span>
           </li>
           <li>
             <span className="Temperature" id="temp">
-              {" "}
+              25{" "}
             </span>
             <span className="Unit">
               {" "}
-              <a id="celsius">°C</a> | <a id="fahrenheit">F</a>
+              <a id="celsius">°C</a> | <a id="fahrenheit">°F</a>
             </span>
           </li>
         </ul>
@@ -27,10 +30,10 @@ export default function Temperature() {
       <div className="Col-6">
         <ul className="Details">
           <li>
-            Humidity <span id="humidity"></span>%
+            Humidity <span id="humidity">65</span>%
           </li>
           <li>
-            Wind <span id="wind"></span>km/h
+            Wind <span id="wind">2</span>km/h
           </li>
         </ul>
       </div>
