6cfac29c8341806bc4877dfede441d6eb9c56679
Simona-de <57255935+Simona-de@users.noreply.github.com>
2019-11-25 20:44:47 +0100

    added API temperature and current date

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import React from "react";
-import Temperature from "./Temperature";
+import Temperature from "./Temperature.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="Weather-html">
-        <h1 id="city">Tokyo</h1>
-        <p id="date">Monday 16:00</p>
-        <form id="enter-city">
-          <input
-            type="text"
-            placeholder="enter city"
-            id="city-input"
-            autoComplete="off"
-          />
-          <input type="submit" value="search" />
-        </form>
-        <button id="current-Loc">current location</button>
-
-        <Temperature />
-
-        <br />
-        <br />
-        <br />
-        <div className="row weather-forecast" id="forecast"></div>
-        <small>
-          <a href="https://github.com/Simona-de/react-appnp" target="blank">
-            {" "}
-            Oper source code
-          </a>{" "}
-          by Simona Delogu
-        </small>
-      </div>
+      <Temperature city="Tokyo" />
+      <br />
+      <br />
+      <br />
+      <div className="row weather-forecast" id="forecast"></div>
+      <small>
+        <a href="https://github.com/Simona-de/react-appnp" target="blank">
+          {" "}
+          Oper source code
+        </a>{" "}
+        by Simona Delogu
+      </small>
     </div>
   );
 }
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,42 +1,77 @@
-import React from "react";
-import App from "./App.css";
+import React, { useState } from "react";
+import "./TemperatureC.css";
+import CorrectedDate from "./CorrectedDate";
 import axios from "axios";
 
-export default function Temperature() {
-  const Apikey = "7012c7fcda50068220d3e6f3e6ec7ff3";
-  return (
-    <div className="Row">
-      <div className="Col-6">
-        <ul>
-          <li>
-            <img
-              src="http://openweathermap.org/img/wn/10d@2x.png"
-              alt=""
-              id="icon"
-            />
-            <span id="sky">clear Sky</span>
-          </li>
-          <li>
-            <span className="Temperature" id="temp">
-              25{" "}
-            </span>
-            <span className="Unit">
-              {" "}
-              <a id="celsius">°C</a> | <a id="fahrenheit">°F</a>
-            </span>
-          </li>
-        </ul>
+export default function Temperature(props) {
+  const [weather, setWeather] = useState({ ready: false });
+  const [city, setCity] = useState(props.City);
+
+  function handleResponse(response) {
+    setWeather({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000)
+    });
+  }
+
+  if (weather.ready) {
+    return (
+      <div className="Weather-html">
+        <h1 id="city">{props.city}</h1>
+        <p id="date">
+          <CorrectedDate date={weather.date} />
+        </p>
+        <form id="enter-city">
+          <input
+            type="text"
+            placeholder="enter city"
+            id="city-input"
+            autoComplete="off"
+          />
+          <input type="submit" value="search" />
+        </form>
+        <button id="current-Loc">current location</button>
+        <div className="Row">
+          <div className="Col-6">
+            <ul>
+              <li>
+                <img src="{weather.icon}" alt="" id="icon" />
+                <span id="sky">{weather.description}</span>
+              </li>
+              <li>
+                <span className="Temperature" id="temp">
+                  {weather.temperature}{" "}
+                </span>
+                <span className="Unit">
+                  {" "}
+                  <a id="celsius">°C</a> | <a id="fahrenheit">°F</a>
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div className="Col-6">
+            <ul className="Details">
+              <li>
+                Humidity <span id="humidity">{weather.humidity}</span>%
+              </li>
+              <li>
+                Wind <span id="wind">{weather.wind}</span>km/h
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="Col-6">
-        <ul className="Details">
-          <li>
-            Humidity <span id="humidity">65</span>%
-          </li>
-          <li>
-            Wind <span id="wind">2</span>km/h
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    let Apikey = "7012c7fcda50068220d3e6f3e6ec7ff3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${Apikey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Searching..";
+  }
 }
